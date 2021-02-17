9dd5377a923904679a7ae14d2cf33f37eb9a6008
Joshua-Talltree <73854373+Joshua-Talltree@users.noreply.github.com>
2021-02-16 19:34:04 -0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using FormAppTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormAppTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            FrontPageModel model = new FrontPageModel();
+            var message = "Hello Michael";
+            int age = 41;
+            string LastName = "Talltree";
+            string FirstName = "Joshua";
+            string homeTown = "San Antonio";
+            string homeState = "Texas";
+         
+            
+            model.MyMessage = message;
+            model.Age = age;
+            model.LastName = LastName;
+            model.FirstName = FirstName;
+            model.homeTown = homeTown;
+            model.homeState = homeState;
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/FrontPageModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormAppTest.Models
+{
+    public class FrontPageModel
+    {
+        public string MyMessage { get; set; }
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string AboutMe { get; set; }
+        public string homeTown { get; set; }
+        public string homeState { get; set; }
+    }
+}
